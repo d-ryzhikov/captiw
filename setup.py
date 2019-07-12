235514fdf3215e9b8f720a4725f7b1430686d591
Dmitry Ryzhikov <d.ryzhykau@gmail.com>
2019-07-12 03:46:09 +0300

    feat: dev environment setup
    chore: ignore eggs
    fix: requirements fixes
    feat: read package requirements and version
    chore: pre-commit config
    feat: install packages for dev

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@ from setuptools import find_packages, setup
 
 LICENSE_CLASSIFIER = "License :: OSI Approved :: MIT License"
 
+with open("VERSION", "r") as f:
+    version = f.read().strip()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().split()
+
 setup(
     name="captiw",
+    version=version,
     packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
     author="Dmitry Ryzhikov",
     author_email="d.ryzhykau@gmail.com",
     license=LICENSE_CLASSIFIER,
